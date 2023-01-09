ed9a1e836420b5fdf620dac1c2449b4b44430c8b
Alexey Osychenko <alexeyosychencko@gmail.com>
2023-01-09 18:03:31 +0200

    c modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { OutletModule } from './outlet/outlet.module';
+import { ReviewModule } from './review/review.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, OutletModule, ReviewModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
