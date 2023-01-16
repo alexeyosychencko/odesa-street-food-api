4d59d320d75d69906009231d07b9d349c79f3023
Alexey Osychenko <alexeyosychencko@gmail.com>
2023-01-16 23:17:29 +0200

    c outlet controller

--- src/outlet/outlet.controller.ts
+++ src/outlet/outlet.controller.ts
@@ -3,45 +3,65 @@ import {
   Controller,
   Delete,
   Get,
-  HttpCode,
+  HttpException,
+  HttpStatus,
   Param,
   Patch,
   Post,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { CreateOutletDto } from './dto/create-outlet.dto';
-import { FindOutlet } from './dto/find.outlet.dto';
-import { OutletDocument } from './outlet.schema';
+import { OutletService } from './outlet.service';
 
 @Controller('outlet')
 export class OutletController {
+  constructor(private readonly outletService: OutletService) {}
+
   @Post('create')
-  async create(@Body() dto: CreateOutletDto) {
-    //
-  }
-
-  @Get(':id')
-  async get(@Param('id') id: string) {
-    //
-  }
-
-  @Get('outlets')
-  async getAll() {
-    //
+  @UseInterceptors(FileInterceptor('image'))
+  async create(@Body() dto: CreateOutletDto, @UploadedFile() image) {
+    return await this.outletService.create(dto, image);
   }
 
   @Delete(':id')
   async delete(@Param('id') id: string) {
-    //
+    const deleted = await this.outletService.delete(id);
+    if (!deleted) {
+      throw new HttpException('Not found outlet.', HttpStatus.NOT_FOUND);
+    }
   }
 
   @Patch(':id')
-  async patch(@Param('id') id: string, @Body() dto: OutletDocument) {
-    //
+  async patch(
+    @Param('id') id: string,
+    @Body() dto: CreateOutletDto,
+    @UploadedFile() image,
+  ) {
+    // TODO: check is this user has access to update this
+    const updatedOutlet = await this.outletService.update(id, dto, image);
+    if (!updatedOutlet) {
+      throw new HttpException('Not found outlet.', HttpStatus.NOT_FOUND);
+    }
   }
 
-  @HttpCode(200)
-  @Post('find')
-  async find(@Body() dto: FindOutlet) {
-    //
+  @Get(':id')
+  async get(@Param('id') id: string) {
+    const outlet = await this.outletService.findById(id);
+    if (!outlet) {
+      throw new HttpException('Not found outlet.', HttpStatus.NOT_FOUND);
+    }
+    return outlet;
+  }
+
+  @Get('outlets')
+  async getAll() {
+    return await this.outletService.findAll();
+  }
+
+  @Get(':userId')
+  async getByUserId(@Param('userId') userId: string) {
+    return await this.outletService.findByUserId(userId);
   }
 }
